68fbd9d5e0488321a12a7c35d7d2121a33e5163c
Vanderlândio Z. da Rocha <vandodev@gmail.com>
2023-01-05 22:35:21 -0300

    Implementado os metodos inserir RegistrarNovoUsuario e ListarToasAsContas

--- byte-bank/Program.cs
+++ byte-bank/Program.cs
@@ -7,16 +7,39 @@
         {
             Console.WriteLine("1 - Inserir novo usuário");
             Console.WriteLine("2 - Deletar um usuário");
-            Console.WriteLine("3 - Detalhes de um usuário");
-            Console.WriteLine("4 - Inserir novo usuário");
+            Console.WriteLine("3 - Listar todas as contas registradas");
+            Console.WriteLine("4 - Detalhes de um usuário");
+            Console.WriteLine("5 - Inserir novo usuário");
             Console.WriteLine("0 - Para sair");
             Console.Write("Digite a opção desejada: ");
+        }
+
+        static void RegistrarNovoUsuario(List<string> cpfs, List<string> titulares, List<double> saldos)
+        {
+            Console.WriteLine("Digite o cpf: ");
+            cpfs.Add(Console.ReadLine());
+            Console.WriteLine("Digite o nome: ");
+            titulares.Add(Console.ReadLine());
+            saldos.Add(0);
+        }
+
+        static void ListarTodasAsContas(List<string> cpfs, List<string> titulares, List<double> saldos)
+        {
+            for(int i = 0; i < cpfs.Count; i++)
+            {
+                Console.WriteLine($"CPF = {cpfs[i]} | Titular = {titulares[i]} | Saldo = R${saldos[i]:F2}");
+            }
         }
         public static void Main(string[] args) {
 
             Console.WriteLine("Antes de de começar a usar, vamos configurar alguns valores.");
-            Console.WriteLine("Digite a quantidade de usuários: ");
-            int quantidadeDeUsuarios = int.Parse(Console.ReadLine());
+
+
+            //dados
+            List<string> cpfs = new List<string>();
+            List<string> titulares = new List<string>();
+            List<double> saldos = new List<double>();
+
 
             int option;
 
@@ -31,15 +54,13 @@
                 {
                     case 0: Console.WriteLine("Encerrando programa!");
                         break;
-                    case 1: Console.WriteLine("Faz o que é pedido na opção 1");
+                    case 1:
+                        RegistrarNovoUsuario(cpfs, titulares, saldos);
                         break;
                     case 2: Console.WriteLine("Faz o que é pedido na opção 2");
                         break;
                     case 3:
-                        Console.WriteLine("Faz o que é pedido na opção 3");
-                        break;
-                    case 4:
-                        Console.WriteLine("Faz o que é pedido na opção 4");
+                        ListarTodasAsContas(cpfs, titulares, saldos);
                         break;
                 }
 
@@ -48,5 +69,6 @@
 
             } while (option != 0);
         }
+
     }
 }
